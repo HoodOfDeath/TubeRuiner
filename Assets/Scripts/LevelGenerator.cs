5cf53ca6c73418f14b1eb47e1ab264c5a80ba396
HoodOfDeath <36995266+HoodOfDeath@users.noreply.github.com>
2018-03-09 11:44:58 +0300

    Initial commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject tunnelPrefab;
+    public GameObject firstTunnel;
+
+    const int startTunnelCount = 40;
+
+    public Transform[] tunnels = new Transform[startTunnelCount];
+
+    float zOffset = 2.1f;
+
+    public int currentTunnelIndex = 1;
+
+    public void GenerateNewTunnel()
+    {
+        Transform previousTunnelTransform;
+        if (currentTunnelIndex == 0)
+        {
+            previousTunnelTransform = tunnels[tunnels.Length - 1];
+        }
+        else
+        {
+            previousTunnelTransform = tunnels[currentTunnelIndex - 1];
+        }
+
+        GameObject currentTunnel = Instantiate(tunnelPrefab,
+        new Vector3(previousTunnelTransform.position.x, previousTunnelTransform.position.y, previousTunnelTransform.position.z + zOffset),
+        new Quaternion(previousTunnelTransform.rotation.x, previousTunnelTransform.rotation.y, previousTunnelTransform.rotation.z, previousTunnelTransform.rotation.w));
+        tunnels[currentTunnelIndex] = currentTunnel.transform;
+        currentTunnel.name += currentTunnelIndex;
+
+        if (currentTunnelIndex+1>tunnels.Length-1)
+        {
+            currentTunnelIndex = 0;
+        }
+        else
+        {
+            currentTunnelIndex++;
+        }
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        tunnels[0] = firstTunnel.transform;
+        for (int i = 1; i <= startTunnelCount; i++)
+        {
+            GenerateNewTunnel();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public LevelGenerator levelGenerator;
+
+    public float speed = 3f;
+    public float speedAdd = 0.1f;
+    public float rotationSpeed = 100f;
+
+    public int targetTunnelIndex = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * Input.GetAxis("Horizontal") * rotationSpeed);
+
+        Transform targetTunnel;
+        if (levelGenerator.currentTunnelIndex<levelGenerator.tunnels.Length-1)
+        {
+            targetTunnel = levelGenerator.tunnels[levelGenerator.currentTunnelIndex + 1];
+        }
+        else
+        {
+            targetTunnel = levelGenerator.tunnels[0];
+        }
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetTunnel.position, step);
+        speed += speedAdd * Time.deltaTime;
+
+        if (transform.position==targetTunnel.position)
+        {
+            targetTunnelIndex++;
+            levelGenerator.GenerateNewTunnel();
+        }
+    }
+}
