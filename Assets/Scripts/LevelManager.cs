d0efca7faeeeb46183bc9bdbb3bb9ed003bf5add
HoodOfDeath <36995266+HoodOfDeath@users.noreply.github.com>
2018-03-10 13:39:00 +0300

    Comlete merge
    Merged Hood's and DrReb's branches

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public Generator TubeGenerator;
+    public GameObject FirstTunnel;
+    public Generator ObstacleGenerator;
+    public GameObject Player;
+
+    float zOffset = 2*2.1f;
+    Vector3 currentSectionPos;
+    Vector3 playerDestination;
+    Quaternion currentRotation;
+    GameObject currentTunnel;
+    GameObject currentObstacle;
+    int tunnelsPassed = 0;
+    float liveTime = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerDestination = FirstTunnel.transform.position;
+        currentSectionPos = FirstTunnel.transform.position;
+        currentRotation = FirstTunnel.transform.rotation;
+        for (int i = 0; i < 10; i++)
+        {
+            currentSectionPos.z += zOffset;
+            currentTunnel = TubeGenerator.Generate(currentSectionPos, currentRotation);
+            Destroy(currentTunnel, liveTime);
+            liveTime += 1f;
+        }
+        Destroy(FirstTunnel, 1f);
+        liveTime = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Player.transform.position.z >= playerDestination.z)
+        {
+            playerDestination.z += zOffset;
+            tunnelsPassed++;
+            tunnelsPassed %= 5;
+            currentSectionPos.z += zOffset;
+            currentTunnel = TubeGenerator.Generate(currentSectionPos, currentRotation);
+            Destroy(currentTunnel, liveTime);
+            if (tunnelsPassed == 3)
+            {
+                currentObstacle = ObstacleGenerator.Generate(currentSectionPos, currentRotation);
+                Destroy(currentObstacle, liveTime);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/TrashGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TrashGenerator : Generator
+{
+    public GameObject TrashPrefab;
+
+    Vector3[] randVect = { new Vector3(2, 0, 0), new Vector3(-2, 0, 0), new Vector3(0, 2, 0), new Vector3(0, -2, 0), new Vector3(1.4142f, 1.4142f, 0), new Vector3(-1.4142f, 1.4142f, 0), new Vector3(1.4142f, -1.4142f, 0), new Vector3(-1.4142f, -1.4142f, 0) };
+    Vector3 rotation = new Vector3(0, 0, 45);
+
+    public override GameObject Generate(Vector3 position, Quaternion Rotation)
+    {
+        //Quaternion rotation = new Quaternion(Rotation.x, Rotation.y, Rotation.z, Rotation.w);
+        System.Random rnd = new System.Random();
+        int pos = rnd.Next(0, 7);
+        GameObject currentObstacle = Instantiate(TrashPrefab, position + (randVect[pos] * 2), Rotation);
+        if (pos>3)
+        {
+            currentObstacle.transform.eulerAngles = rotation;
+        }
+        return currentObstacle;
+    }
+}
