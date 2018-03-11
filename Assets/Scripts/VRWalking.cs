289025523235cf9a48dde111d0e77508da5e9259
DrReb <36999733+DrReb@users.noreply.github.com>
2018-03-11 11:23:53 +0300

    Clearing

--- Assets/Scripts/VRWalking.cs
+++ Assets/Scripts/VRWalking.cs
@@ -2,16 +2,9 @@
 
 public class VRWalking : MonoBehaviour
 {
-    //public Transform vrCamera;
-    //private Camera MainCamera;
+    private Camera MainCamera;
     const float scale = 10f;
     public float Speed = 1f;
-    public float XYSpeed = 5f;
-    public bool mod = true;
-    bool moveForward = true;
-    RaycastHit hit;
-    Mod currentMod;
-    public Camera MainCam;
 
     private CharacterController Player;
 
@@ -19,54 +12,35 @@ public class VRWalking : MonoBehaviour
     {
         
         //Player = GetComponent<CharacterController>();
-        if (mod)
-        {
-            currentMod = MoveBySight;
-        }
-        else
-        {
-            currentMod = Engage;
-        }
-        //MainCamera = Camera.main;
+        MainCamera = Camera.main;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            moveForward = !moveForward;
-        currentMod(moveForward, Speed);
+        MoveBySight(Speed);
     }
 
-    delegate void Mod(bool moveForvard, float Speed);
+    //delegate void Mod(bool moveForvard, float Speed);
 
-    void Engage(bool moveForvard, float Speed)
+    /*void FirstPersonMovement(bool moveForvard, float Speed)
     {
         if (moveForward)
         {
             Vector3 gaze = MainCam.transform.forward * Speed * Time.deltaTime;
             Player.Move(gaze);
         }
-    }
+    }//*/
 
     Vector3 PlayerPositionZ = new Vector3(0, 0, 0);
     Vector2 g;
     Vector2 PlayerPositionXY;
-    void MoveBySight(bool moveForvard, float Speed)
+    void MoveBySight(float Speed)
     {
-        if (moveForvard)
-        {
-
-            Vector3 gaze = MainCam.transform.forward;
-            g = new Vector2(gaze.x, gaze.y) * scale;
-            /*if( Physics.Raycast(MainCam.transform.position, gaze, out hit, 20.0f))
-            if( hit.collider.tag == "PlayersController" | hit.collider.tag == "Player")
-            {
-                g = new Vector2( hit.point.x, hit.point.y);
-            }//*/
-            PlayerPositionXY = transform.position;
-            PlayerPositionZ += Vector3.forward * Speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(PlayerPositionXY, g, Speed * Time.deltaTime) + PlayerPositionZ;
-            Debug.Log(gaze);
-        }
+        Vector3 gaze = MainCamera.transform.forward;
+        g = new Vector2(gaze.x, gaze.y) * scale;
+        PlayerPositionXY = transform.position;
+        PlayerPositionZ += Vector3.forward * Speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(PlayerPositionXY, g, Speed * Time.deltaTime * 5) + PlayerPositionZ;
+        Debug.Log(gaze);
     }
 }
